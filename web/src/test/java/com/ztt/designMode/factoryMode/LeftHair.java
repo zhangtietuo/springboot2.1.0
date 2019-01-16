d3d0b256aa2f13be993ecde9aa606886baffb59f
zhangtt <740890845@qq.com>
2019-01-16 10:00:53 +0800

    工厂模式

---
+++ web/src/test/java/com/ztt/designMode/factoryMode/LeftHair.java
@@ -0,0 +1,15 @@
+package com.ztt.designMode.factoryMode;
+
+/**
+ * @Auther: zhangtietuo
+ * @Description:
+ * @Date: 2019/1/15 14:35
+ */
+public class LeftHair implements HairInterface {
+
+
+    @Override
+    public void draw() {
+        System.out.println("=============左偏分的发型=====================");
+    }
+}
---
+++ web/src/test/java/com/ztt/designMode/factoryMode/RightHair.java
@@ -0,0 +1,15 @@
+package com.ztt.designMode.factoryMode;
+
+/**
+ * @Auther: zhangtietuo
+ * @Description:
+ * @Date: 2019/1/15 14:35
+ */
+public class RightHair implements HairInterface {
+
+
+    @Override
+    public void draw() {
+        System.out.println("=============右偏分的发型=====================");
+    }
+}
