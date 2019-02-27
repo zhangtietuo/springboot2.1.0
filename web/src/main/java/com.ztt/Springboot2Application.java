8d1061a71c1b155bdc3f33171f1e1a145a91a429
zhangtt <740890845@qq.com>
2019-02-27 10:53:39 +0800

    spring cloud

--- web/src/main/java/com.ztt/Springboot2Application.java
+++ web/src/main/java/com.ztt/Springboot2Application.java
@@ -8,12 +8,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 @SpringBootApplication
 @MapperScan("com.ztt.mapper")
+@EnableDiscoveryClient
 //@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})  //因为创建项目时候添加了数据库组件，所以autoconfig会去读取数据源配置，而新建的项目还没有配置数据源，所以会导致异常出现
 public class Springboot2Application {
 
