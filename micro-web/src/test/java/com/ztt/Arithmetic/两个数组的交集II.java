1e80d68eb23208cb0eea04ec72e7f9790dbeac56
zhangtietuo <Yangguang0325>
2019-06-27 10:34:44 +0800

    springcloud

---
+++ micro-web/src/test/java/com/ztt/Arithmetic/两个数组的交集II.java
@@ -0,0 +1,18 @@
+package com.ztt.Arithmetic;
+
+/**
+ * @Auther: zhangtietuo
+ * @Description:
+ * @Date: 2019/6/27 10:02
+ */
+public class 两个数组的交集II {
+
+    public int[] intersect(int[] nums1, int[] nums2) {
+        return new int[]{1, 2, 3};
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
