b541a85099d185dae1202eb397ef4a99514f2a29
zhangtietuo <zhangtietuo@wejoydata.com>
2021-10-16 17:06:35 +0800

    聊天室

--- micro-web/src/test/java/com/ztt/ThreadPool/ThreadPool.java
+++ micro-web/src/test/java/com/ztt/ThreadPool/ThreadPool.java
@@ -1,9 +1,9 @@
 package com.ztt.ThreadPool;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import io.netty.util.concurrent.DefaultThreadFactory;
+import lombok.SneakyThrows;
+
+import java.util.concurrent.*;
 
 /**
  * @Auther: zhangtietuo
@@ -14,22 +14,64 @@ public class ThreadPool {
 
 
     public static void main(String[] args) {
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
-                new ArrayBlockingQueue<Runnable>(5));
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 2, 1, TimeUnit.SECONDS, new ArrayBlockingQueue(1), new DefaultThreadFactory("ztt-pool-%d", true), new ThreadPoolExecutor.CallerRunsPolicy());
         executor.execute(new Thread() {
+            @SneakyThrows
             @Override
             public void run() {
-                System.out.println("开始进行");
+                System.out.println(Thread.currentThread().getName() + "A-");
+                Thread.sleep(5000);
+                System.out.println("A");
             }
         });
-        /*for(int i=0;i<15;i++){
-            MyTask myTask = new MyTask(i);
-            executor.execute(myTask);
-            System.out.println("线程池中线程数目："+executor.getPoolSize()+"，队列中等待执行的任务数目："+
-                    executor.getQueue().size()+"，已执行玩别的任务数目："+executor.getCompletedTaskCount());
-        }
-        executor.shutdown();*/
-        Executors.newCachedThreadPool();
+        executor.execute(new Thread() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "B-");
+                Thread.sleep(5000);
+                System.out.println("B");
+            }
+        });
+        executor.execute(new Thread() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "C-");
+                Thread.sleep(5000);
+                System.out.println("C");
+            }
+        });
+        executor.execute(new Thread() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + "D-");
+                Thread.sleep(5000);
+                System.out.println("D");
+            }
+        });
+        System.out.println("xixi");
+
+
+
+
+//        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 10, 200, TimeUnit.MILLISECONDS,
+//                new ArrayBlockingQueue<Runnable>(5));
+//        executor.execute(new Thread() {
+//            @Override
+//            public void run() {
+//                System.out.println("开始进行");
+//            }
+//        });
+//        /*for(int i=0;i<15;i++){
+//            MyTask myTask = new MyTask(i);
+//            executor.execute(myTask);
+//            System.out.println("线程池中线程数目："+executor.getPoolSize()+"，队列中等待执行的任务数目："+
+//                    executor.getQueue().size()+"，已执行玩别的任务数目："+executor.getCompletedTaskCount());
+//        }
+//        executor.shutdown();*/
+//        Executors.newCachedThreadPool();
     }
 
     static class MyTask implements Runnable {
